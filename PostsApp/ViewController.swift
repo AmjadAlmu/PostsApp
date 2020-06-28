5c848fb8400fc8fb8de65bd561b34d6c4b1220d8
Amjad Almuwallad <amjad.almuwallad@gmail.com>
2020-06-28 17:10:04 +0300

    Initial Commit

---
+++ PostsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostsApp
+//
+//  Created by Amjad Almuwallad on 28/06/2020.
+//  Copyright © 2020 Amjad Almuwallad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
